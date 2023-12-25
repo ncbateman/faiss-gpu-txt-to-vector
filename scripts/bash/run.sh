b0c1ad2238ed4f2e50f01cc18dc91379ac8ec795
Nicholas Bateman <ncbateman@gmail.com>
2023-12-25 17:10:34 +0000

    moved to cli flags

--- scripts/bash/run.sh
+++ scripts/bash/run.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-# Read values from the YAML file
-tokenizer_model=$(yq eval '.tokenizer_model' ./config/config.yaml)
-index_output=$(yq eval '.index_output' ./config/config.yaml)
-txt_input_dir=$(yq eval '.txt_input_dir' ./config/config.yaml)
+# Default values
+default_tokenizer_model="./models/"
+default_index_output="./indexes/"
+default_txt_input_dir="./input/"
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -t|--tokenizer-model) tokenizer_model="$2"; shift ;;
+        -i|--index-output) index_output="$2"; shift ;;
+        -d|--txt-input-dir) txt_input_dir="$2"; shift ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Set default values if not provided
+tokenizer_model=${tokenizer_model:-$default_tokenizer_model}
+index_output=${index_output:-$default_index_output}
+txt_input_dir=${txt_input_dir:-$default_txt_input_dir}
 
 # Build Docker images
 docker build -f ./docker/base/Dockerfile -t faiss-gpu-txt-to-vector:latest .
 docker build -f ./docker/rag-index-builder/Dockerfile -t faiss-gpu-txt-to-vector:latest .
 
 # Stop and remove existing container
-docker stop faiss-gpu-txt-to-vector
-docker rm faiss-gpu-txt-to-vector
+docker stop faiss-gpu-txt-to-vector 2>/dev/null
+docker rm faiss-gpu-txt-to-vector 2>/dev/null
 
-# Run the Docker container with the volumes from the YAML configuration
+# Run the Docker container with the volumes
 docker run \
     --name faiss-gpu-txt-to-vector \
     --gpus all \
