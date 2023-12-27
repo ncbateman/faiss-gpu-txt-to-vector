bd66f1d8f31ef7225559317de4aaa39239b39ecd
Nicholas Bateman <ncbateman@gmail.com>
2023-12-27 19:09:28 +0000

    test - not plain

--- scripts/bash/test.sh
+++ scripts/bash/test.sh
@@ -1,5 +1,5 @@
 # Build Docker images
-docker build --progress=plain -f ./docker/base/Dockerfile -t faiss-gpu-txt-to-vector:latest .
+docker build -f ./docker/base/Dockerfile -t faiss-gpu-txt-to-vector:latest .
 docker stop faiss-gpu-txt-to-vector-test
 docker rm faiss-gpu-txt-to-vector-test
 docker build -f ./docker/test/Dockerfile -t faiss-gpu-txt-to-vector-test:latest .
