6ab85a79719227c2efacf307a344ef932de17c4f
Nicholas Bateman <ncbateman@gmail.com>
2023-12-27 12:30:22 +0000

    test docker build

--- scripts/bash/test.sh
+++ scripts/bash/test.sh
@@ -1,1 +1,6 @@
-docker build -f ./docker/gpt/Dockerfile -t home-gpt:latest .
+# Build Docker images
+docker build -f ./docker/base/Dockerfile -t faiss-gpu-txt-to-vector:latest .
+docker stop faiss-gpu-txt-to-vector-test
+docker rm faiss-gpu-txt-to-vector-test
+docker build -f ./docker/test/Dockerfile -t faiss-gpu-txt-to-vector-test:latest .
+docker run --name faiss-gpu-txt-to-vector-test faiss-gpu-txt-to-vector-test:latest
