c4c5ecb165467d9ead31fb7a5a9cb4431fda68de
Nicholas Bateman <ncbateman@gmail.com>
2023-12-27 22:47:31 +0000

    test - post injection

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,9 +4,9 @@ import app
 from index.builder import IndexBuilder
 
 @pytest.fixture
-def mock_index_builder():
-    with patch('index.builder.IndexBuilder', autospec=True) as mock_builder:
-        yield mock_builder
+def mock_index_builder_create_index():
+    with patch.object(IndexBuilder, 'create_index') as mock:
+        yield mock
 
 @pytest.fixture
 def mock_tokenizer():
@@ -19,28 +19,22 @@ def mock_model():
         yield mock
 
 @pytest.fixture
-def mock_torch():
+def mock_torch_cuda():
     with patch('torch.cuda.is_available', return_value=True):
         yield
 
-def test_main_success(mock_index_builder, mock_tokenizer, mock_model, mock_torch):
-    mock_builder_instance = mock_index_builder.return_value
-    mock_builder_instance.create_index.return_value = None
-
-    with patch('logging.Logger.info') as mock_logging_info:
+def test_main_success(mock_index_builder_create_index, mock_tokenizer, mock_model, mock_torch_cuda):
+    with patch('logging.info') as mock_logging_info:
         app.main()
-        # Check if IndexBuilder was initialized
-        mock_index_builder.assert_called_once()
         # Check if create_index was called
-        mock_builder_instance.create_index.assert_called_once()
+        mock_index_builder_create_index.assert_called_once()
         # Check if success log was written
         mock_logging_info.assert_called_with("Index building process completed successfully.")
 
-def test_main_exception(mock_index_builder, mock_tokenizer, mock_model, mock_torch):
-    mock_builder_instance = mock_index_builder.return_value
-    mock_builder_instance.create_index.side_effect = Exception("Test Error")
+def test_main_exception(mock_index_builder_create_index, mock_tokenizer, mock_model, mock_torch_cuda):
+    mock_index_builder_create_index.side_effect = Exception("Test Error")
 
-    with patch('logging.Logger.error') as mock_logging_error, pytest.raises(Exception):
+    with patch('logging.error') as mock_logging_error, pytest.raises(Exception):
         app.main()
         # Check if error log was written
         mock_logging_error.assert_called_with("An error occurred: Test Error")
